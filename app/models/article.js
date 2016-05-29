d20e0dd89a03ae3bbd8a05b08d9f6259ab080aa2
Jonatan Ramhöj <jonatan.ramhoj@netrelations.com>
2016-05-29 23:23:23 +0200

    Paging WIP.

--- app/models/article.js
+++ app/models/article.js
@@ -1,5 +1,6 @@
 // Module dependencies
 var mongoose = require('mongoose');
+var mongoosePaginate = require('mongoose-paginate');
 var Schema  = mongoose.Schema;
 
 // Database schema for model
@@ -14,6 +15,9 @@ var articleSchema = new Schema({
 		ref: 'user'
 	}
 });
+
+// Add pagination functionality to article model
+articleSchema.plugin(mongoosePaginate);
 
 // Create the article model
 var article = mongoose.model('article', articleSchema);
